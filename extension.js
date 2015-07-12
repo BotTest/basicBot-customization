9e5a14f8540ebefe6563b60cb2a25324cec2ee0c
BotTest <sjahsjasa@hotmail.com>
2015-07-12 01:05:36 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -61,7 +61,7 @@
         chatLink: "https://rawgit.com/BotTest/BotTestFu/master/lang/pt.json",
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupEmoji: true, // true or false
         autowoot: true,
         autoskip: false,
         smartSkip: true,
