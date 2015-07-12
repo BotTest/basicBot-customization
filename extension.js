4063a4e6a6821965fc5630e18d23e10e05f7d34d
BotTest <sjahsjasa@hotmail.com>
2015-07-12 12:52:50 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -69,6 +69,7 @@
         maximumAfk: 60,
         afkRemoval: true,
         maximumDc: 60,
+        thorInterval: 10,
         bouncerPlus: true,
         blacklistEnabled: true,
         lockdownEnabled: false,
